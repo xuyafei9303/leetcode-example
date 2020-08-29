dd793deb221c79a75abe5d6186c414ee7611db9a
xyf <xuyafei9303@gmail.com>
2020-08-29 19:02:15 +0800

    整理leetcode上的题目解析，长期更新

---
+++ src/com/ixyf/simple/Reverse.java
@@ -0,0 +1,38 @@
+package com.ixyf.simple;
+
+/**
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ *
+ * 示例 1:
+ *
+ * 输入: 123
+ * 输出: 321
+ *  示例 2:
+ *
+ * 输入: -123
+ * 输出: -321
+ * 示例 3:
+ *
+ * 输入: 120
+ * 输出: 21
+ */
+public class Reverse {
+
+    public static int reverse(int x) {
+        int ret = 0;
+        while (x != 0) {
+            int t = x % 10;
+            int newRet = ret * 10 + t;
+            if ((newRet - t) / 10 != ret) {
+                return 0;
+            }
+            ret = newRet;
+            x = x / 10;
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        reverse(-6392929);
+    }
+}
